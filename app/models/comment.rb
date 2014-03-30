a458e7302b9975941973158d006895fbfd5c1c36
Chris Mangano <chris.mangano@me.com>
2014-03-29 20:45:58 -0400

    Added comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,14 +6,14 @@ class Comment < ActiveRecord::Base
 	#methods can be passed in as symbols
 	validate :article_should_be_published
 
-	after_create :email_article_author  
+	#after_create :email_article_author  
 
 
 	def article_should_be_published
 		errors.add(:article_id, "is not published yet") if article && !article.published?
 	end
 
-	def email_article_author
-		puts "We will notify #{article.user.email} the author in Chapter 9 or 10"
-	end
+	#def email_article_author
+	#	puts "We will notify #{article.user.email} the author in Chapter 9 or 10"
+	#end
 end
